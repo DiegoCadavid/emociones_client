6f33951ae0ede5e7a272b9c818c573355989c435
DiegoCadavid <dcmgames50@gmail.com>
2022-11-13 19:13:05 -0500

    feat: change style of home button from titles

--- src/components/placeholders/Title.jsx
+++ src/components/placeholders/Title.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Title = ({ title = "titulo", subtitle = "" }) => {
   return (
-    <div className="text-center">
-      <h2 className="text-primary-100 font-title text-3xl uppercase dark:text-zinc-100">
-        {title}
-      </h2>
-      <pre className="font-subtitle italic leading-tight mt-1 mb-2 dark:text-zinc-300">{subtitle}</pre>
-      <Link to="/home" className="hover:underline text-primary-100 dark:text-zinc-100">inicio</Link>
+    <div className="text-center flex flex-col gap-3 items-center">
+      <div>
+        <h2 className="text-primary-100 font-title text-3xl uppercase dark:text-zinc-100">
+          {title}
+        </h2>
+        <pre className="font-subtitle italic leading-tight mt-1 dark:text-zinc-300">
+          {subtitle}
+        </pre>
+      </div>
+      <Link to="/home" className="btn w-fit">
+        inicio
+      </Link>
     </div>
   );
 };
